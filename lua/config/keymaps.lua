cabc7e6390a5319e740c5a08a88a075845ecd98b
Huynh Duc Dung <dunghd.it@gmail.com>
2023-02-22 23:24:41 +0800

    chore: add some useful keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,15 @@ local opts = { silent = true }
 
 -- Close buffers
 keymap("n", "<S-q>", "<cmd>Bdelete!<CR>", opts)
+
+-- Copy whole file content to clipboard with C-c
+keymap("n", "<C-c>", ":%y+<CR>", opts)
+
+-- Move live up or down
+-- moving
+keymap("n", "<A-Down>", ":m .+1<CR>", opts)
+keymap("n", "<A-Up>", ":m .-2<CR>", opts)
+keymap("i", "<A-Down>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("i", "<A-Up>", "<Esc>:m .-2<CR>==gi", opts)
+keymap("v", "<A-Down>", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "<A-Up>", ":m '<-2<CR>gv=gv", opts)
