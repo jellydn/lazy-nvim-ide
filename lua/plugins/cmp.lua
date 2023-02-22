f0993c8fac41d7b27d7f67df062366dfecc3a55b
Huynh Duc Dung <dunghd.it@gmail.com>
2023-02-22 18:26:56 +0800

    feat: add cmp emoji

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
+    end,
+  },
+}
