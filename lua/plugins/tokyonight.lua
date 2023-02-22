3b23a27e0ad9beeace9df159e95d72867f0fd14a
Huynh Duc Dung <dunghd.it@gmail.com>
2023-02-22 18:37:08 +0800

    feat: make tokyo transparent

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
