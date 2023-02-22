8ce5f7e8f800adacb795cd5b8dd622c4a9ab6773
Huynh Duc Dung <dunghd.it@gmail.com>
2023-02-22 23:01:44 +0800

    feat: add outline symbol

---
+++ lua/plugins/outline.lua
@@ -0,0 +1,35 @@
+-- Silent keymap option
+local opts = { silent = true }
+
+return {
+  "stevearc/aerial.nvim",
+  event = "VeryLazy",
+  config = function()
+    local status_ok, aerial = pcall(require, "aerial")
+    if not status_ok then
+      return
+    end
+
+    -- Toggle Outline
+    vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>", opts)
+
+    aerial.setup({
+
+      -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+      on_attach = function(bufnr)
+        -- Jump forwards/backwards with '{' and '}'
+        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+
+        vim.keymap.set("n", "<leader>fi", "<cmd>Telescope aerial<CR>", opts)
+      end,
+    })
+
+    local tele_status_ok, telescope = pcall(require, "telescope")
+    if not tele_status_ok then
+      return
+    end
+
+    telescope.load_extension("aerial")
+  end,
+}
