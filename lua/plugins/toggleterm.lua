be20610177684f7f11a6a1ce7b0d24f40add74bc
Huynh Duc Dung <dunghd.it@gmail.com>
2023-02-23 17:35:20 +0800

    feat: add toggle term

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      size = 20,
+      open_mapping = [[<c-\>]],
+      hide_numbers = true,
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      insert_mappings = true,
+      persist_size = true,
+      direction = "float",
+      close_on_exit = true,
+      shell = vim.o.shell,
+      float_opts = {
+        border = "curved",
+      },
+    },
+    keys = {
+      {
+        "<c-\\>",
+        desc = "Toggle term",
+      },
+    },
+  },
+}
