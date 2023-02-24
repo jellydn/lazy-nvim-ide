3b03144858084be7df0c9b9c9db7758c5e414f16
Huynh Duc Dung <dunghd.it@gmail.com>
2023-02-24 22:54:07 +0800

    chore: disable neoconf plugin

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,7 @@
 return {
   --- disable nvim-notify
   { "rcarriga/nvim-notify", enabled = false },
+  -- Disable until this issue has been fixed https://github.com/LazyVim/LazyVim/issues/295
+  { "folke/neoconf.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
 }
