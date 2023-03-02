69f2582dc2ea9fc0410298571470fb674ca05350
Huynh Duc Dung <dunghd.it@gmail.com>
2023-03-02 23:21:31 +0800

    fix: conflict lsp for typescript vs deno

--- lua/plugins/deno.lua
+++ lua/plugins/deno.lua
@@ -1,6 +1,45 @@
 return {
   {
     "sigmasd/deno-nvim",
-    enabled = false,
+    keys = {
+      -- add a keymap to browse plugin files
+      {
+        "<leader>dr",
+        function()
+          vim.lsp.codelens.refresh()
+          vim.lsp.codelens.run()
+        end,
+        desc = "Deno test with code lens",
+      },
+    },
+    opts = {
+      server = {
+        root_dir = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc"),
+        settings = {
+          deno = {
+            inlayHints = {
+              parameterNames = {
+                enabled = "all",
+              },
+              parameterTypes = {
+                enabled = true,
+              },
+              variableTypes = {
+                enabled = true,
+              },
+              propertyDeclarationTypes = {
+                enabled = true,
+              },
+              functionLikeReturnTypes = {
+                enabled = true,
+              },
+              enumMemberValues = {
+                enabled = true,
+              },
+            },
+          },
+        },
+      },
+    },
   },
 }
