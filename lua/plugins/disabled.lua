1cb993635c082e906667cc92f967e253fcc40f95
Huynh Duc Dung <dunghd.it@gmail.com>
2023-03-02 23:21:42 +0800

    chore: disable search and replace

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,9 @@
 return {
-  --- disable nvim-notify
+  -- disable unused theme
+  { "catppuccin/nvim", enabled = false },
+  -- disable nvim spectrum
+  { "windwp/nvim-spectre", enabled = false },
+  -- disable nvim-notify
   { "rcarriga/nvim-notify", enabled = false },
   -- Disable until this issue has been fixed https://github.com/LazyVim/LazyVim/issues/295
   { "folke/neoconf.nvim", enabled = false },
