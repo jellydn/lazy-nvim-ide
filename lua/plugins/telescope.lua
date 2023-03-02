1ac24834fefc3be06d3f8bfe7578ef148386b543
Huynh Duc Dung <dunghd.it@gmail.com>
2023-03-02 23:21:51 +0800

    feat: add fzf native

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,13 @@
+return {
+  -- add telescope-fzf-native
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+}
