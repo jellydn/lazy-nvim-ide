06b1504ffd31f961ed0ac954a97d01f44bcb4a2a
Huynh Duc Dung <dunghd.it@gmail.com>
2023-03-11 08:46:47 +0800

    chore: add find git keymap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,9 +2,21 @@ return {
   -- add telescope-fzf-native
   {
     "telescope.nvim",
+    keys = {
+      -- add a keymap to browse plugin files
+      -- stylua: ignore
+      {
+        "<leader>fg",
+        function() require("telescope.builtin").git_files() end,
+        desc = "Find Git File",
+      },
+    },
     dependencies = {
       "nvim-telescope/telescope-fzf-native.nvim",
       build = "make",
+      cond = function()
+        return vim.fn.executable("make") == 1
+      end,
       config = function()
         require("telescope").load_extension("fzf")
       end,
