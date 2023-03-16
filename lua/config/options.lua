15e4de34ea20fa7beeb16f072287a0163a0d1a91
Huynh Duc Dung <dunghd.it@gmail.com>
2023-03-16 22:50:07 +0800

    feat: add folding with treesistter

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+-- Folding https://github.com/nvim-treesitter/nvim-treesitter/issues/4152#issuecomment-1419685500
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+vim.o.foldlevelstart = 99
