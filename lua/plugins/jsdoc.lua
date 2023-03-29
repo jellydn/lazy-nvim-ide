65e1146f1e103311f8d67df212d157aa22340b7c
Huynh Duc Dung <dunghd.it@gmail.com>
2023-03-29 20:54:36 +0800

    feat: add jsdoc

---
+++ lua/plugins/jsdoc.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "heavenshell/vim-jsdoc",
+    ft = "javascript,typescript,typescriptreact,svelte",
+    cmd = "JsDoc",
+    keys = {
+      { "<leader>d", "<cmd>JsDoc<cr>", desc = "JsDoc" },
+    },
+    build = "make install",
+  },
+}
