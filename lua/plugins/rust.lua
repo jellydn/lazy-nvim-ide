a9e408031e33f1e8e8d8edec2bea010f093d1dd1
Huynh Duc Dung <dunghd.it@gmail.com>
2023-03-30 21:41:56 +0800

    chore: set keymap for rust on hover

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -10,9 +10,12 @@ return {
         server = {
           on_attach = function(_, bufnr)
             -- Hover actions
-            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-            -- Code action groups
-            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+            vim.keymap.set(
+              "n",
+              "<Leader>ch",
+              rt.hover_actions.hover_actions,
+              { buffer = bufnr, desc = "Rust - Hover actions" }
+            )
           end,
         },
       })
