dbc5a9cd07400bb6c3c07c4f5b8b352704e78f84
Huynh Duc Dung <dunghd.it@gmail.com>
2023-04-01 13:51:07 +0800

    chore: remove toggleterm with lazygit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -43,27 +43,6 @@ if Util.has("null-ls.nvim") then
   keymap("n", "<leader>cn", "<cmd>NullLsInfo<CR>", opts)
 end
 
--- Lazygit with toggleterm
-if Util.has("toggleterm.nvim") then
-  function _G.set_terminal_keymaps()
-    -- vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
-  end
-
-  vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
-  local Terminal = require("toggleterm.terminal").Terminal
-  local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-
-  function _LAZYGIT_TOGGLE()
-    lazygit:toggle()
-  end
-
-  keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
-end
-
 -- LspSaga
 if Util.has("lspsaga.nvim") then
   -- when you use action in finder like open vsplit then you can
