a0b05295fdeb28215072931b9efa2e252108dd69
Huynh Duc Dung <dunghd.it@gmail.com>
2023-04-11 10:39:52 +0800

    feat: chore add keymap for grep word by git

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,36 @@
+local function live_grep_from_project_git_root()
+  local function is_git_repo()
+    vim.fn.system("git rev-parse --is-inside-work-tree")
+
+    return vim.v.shell_error == 0
+  end
+
+  local function get_git_root()
+    local dot_git_path = vim.fn.finddir(".git", ".;")
+    return vim.fn.fnamemodify(dot_git_path, ":h")
+  end
+
+  local opts = {}
+
+  if is_git_repo() then
+    opts = {
+      cwd = get_git_root(),
+    }
+  end
+
+  require("telescope.builtin").live_grep(opts)
+end
+
+local function fallback_to_find_files_if_not_git()
+  local opts = {} -- define here if you want to define something
+  vim.fn.system("git rev-parse --is-inside-work-tree")
+  if vim.v.shell_error == 0 then
+    require("telescope.builtin").git_files(opts)
+  else
+    require("telescope.builtin").find_files(opts)
+  end
+end
+
 return {
   -- add telescope-fzf-native
   {
@@ -7,8 +40,19 @@ return {
       -- stylua: ignore
       {
         "<leader>fg",
-        function() require("telescope.builtin").git_files() end,
+        function()
+          fallback_to_find_files_if_not_git()
+        end,
         desc = "Find Git File",
+      },
+      -- find in file 
+      -- stylua: ignore
+      {
+        "<leader>fw",
+        function()
+          live_grep_from_project_git_root()
+        end,
+        desc = "Find Word In Files",
       },
     },
     dependencies = {
