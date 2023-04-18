ac0f6a96a96126791471fe6b7c85e1d982844b8d
Huynh Duc Dung <dunghd.it@gmail.com>
2023-04-18 12:25:08 +0800

    chore: add keymap for open dashboard

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,6 +12,22 @@ local opts = { silent = true }
 keymap("i", "jj", "<ESC>", opts)
 keymap("i", "jk", "<ESC>", opts)
 
+-- Dashboard
+-- Add keymap to open alpha dashboard
+keymap("n", "<leader>;", function()
+  -- close all open buffers before open dashboard
+  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+    local buftype = vim.api.nvim_buf_get_option(bufnr, "buftype")
+    if buftype ~= "terminal" then
+      vim.api.nvim_buf_delete(bufnr, { force = true })
+    end
+  end
+
+  if Util.has("alpha-nvim") then
+    require("alpha").start(true)
+  end
+end, opts)
+
 -- Close buffers
 if Util.has("mini.bufremove") then
   keymap("n", "<S-q>", function()
