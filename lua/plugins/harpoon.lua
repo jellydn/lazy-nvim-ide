19747f21811a49bb01508ac0c259726290681f95
Huynh Duc Dung <dunghd.it@gmail.com>
2023-05-17 17:40:06 +0800

    feat: set 50 width and heigh for harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -12,13 +12,20 @@ return {
         desc = "Telescope Harpoon Marks",
       },
     },
-    config = function()
+    opts = {
+      menu = {
+        -- 50% width, height
+        width = vim.api.nvim_win_get_width(0) / 2,
+        height = vim.api.nvim_win_get_height(0) / 2,
+      },
+    },
+    config = function(_, options)
       local status_ok, harpoon = pcall(require, "harpoon")
       if not status_ok then
         return
       end
 
-      harpoon.setup({})
+      harpoon.setup(options)
 
       local tele_status_ok, telescope = pcall(require, "telescope")
       if not tele_status_ok then
