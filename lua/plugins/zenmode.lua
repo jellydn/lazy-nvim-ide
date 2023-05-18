65f2e704d66660f11a5d5494193270423ea0064d
Huynh Duc Dung <dunghd.it@gmail.com>
2023-05-19 07:21:36 +0800

    feat: add zen mode

--- lua/plugins/zenmode.lua
+++ lua/plugins/zenmode.lua
@@ -2,6 +2,11 @@ return {
   {
     "folke/zen-mode.nvim",
     cmd = { "ZenMode" },
+    opts = {
+      window = {
+        width = 0.85, -- width will be 85% of the editor width
+      },
+    },
     config = true,
     keys = {
       -- add <leader>cz to enter zen mode
