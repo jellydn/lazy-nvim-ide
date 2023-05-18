ecbb64da791e2837fdb909f9d19931875599b8df
Huynh Duc Dung <dunghd.it@gmail.com>
2023-05-19 07:21:14 +0800

    chore: add copilot to cmp

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,7 +14,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
-    -- { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- { import = "lazyvim.plugins.extras.linting.eslint" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,5 @@
+local enable_copilot_statusline = false
+
 return {
   {
     "github/copilot.vim",
@@ -28,20 +30,22 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
-      local Util = require("lazyvim.util")
-      table.insert(opts.sections.lualine_x, 2, {
-        function()
-          local icon = require("lazyvim.config").icons.kinds.Copilot
-          return icon
-        end,
-        cond = function()
-          local ok, clients = pcall(vim.lsp.get_active_clients, { name = "copilot", bufnr = 0 })
-          return ok and #clients > 0
-        end,
-        color = function()
-          return Util.fg("Special")
-        end,
-      })
+      if enable_copilot_statusline then
+        local Util = require("lazyvim.util")
+        table.insert(opts.sections.lualine_x, 2, {
+          function()
+            local icon = require("lazyvim.config").icons.kinds.Copilot
+            return icon
+          end,
+          cond = function()
+            local ok, clients = pcall(vim.lsp.get_active_clients, { name = "copilot", bufnr = 0 })
+            return ok and #clients > 0
+          end,
+          color = function()
+            return Util.fg("Special")
+          end,
+        })
+      end
     end,
   },
 }
