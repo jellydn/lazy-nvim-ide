e987a502a7826163bf08a744d196f62e052a2e5d
Huynh Duc Dung <dunghd.it@gmail.com>
2023-05-23 20:41:37 +0800

    feat: add some popular themes

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,23 @@
+return {
+  -- add gbprod/nord.nvim colorscheme
+  {
+    "gbprod/nord.nvim",
+  },
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- add nightfox
+  { "EdenEast/nightfox.nvim" },
+
+  -- add night-owl
+  { "haishanh/night-owl.vim" },
+
+  -- add kanagawa
+  { "rebelot/kanagawa.nvim" },
+
+  -- set LazyVim to load colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {},
+  },
+}
