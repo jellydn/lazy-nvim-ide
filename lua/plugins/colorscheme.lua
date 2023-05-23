c4b4b135a1da288c68f825af114daf76737f048b
Huynh Duc Dung <dunghd.it@gmail.com>
2023-05-23 21:31:31 +0800

    feat: lazy load for all color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,18 +2,19 @@ return {
   -- add gbprod/nord.nvim colorscheme
   {
     "gbprod/nord.nvim",
+    lazy = true,
   },
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  { "ellisonleao/gruvbox.nvim", lazy = true },
 
   -- add nightfox
-  { "EdenEast/nightfox.nvim" },
+  { "EdenEast/nightfox.nvim", lazy = true },
 
   -- add night-owl
-  { "haishanh/night-owl.vim" },
+  { "haishanh/night-owl.vim", lazy = true },
 
   -- add kanagawa
-  { "rebelot/kanagawa.nvim" },
+  { "rebelot/kanagawa.nvim", lazy = true },
 
   -- set LazyVim to load colorscheme
   {
