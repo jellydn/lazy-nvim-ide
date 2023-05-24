59949fd976c2213ab7c3aa19cdf9a6fd9e4c9b2f
Huynh Duc Dung <dunghd.it@gmail.com>
2023-05-25 06:13:14 +0800

    feat: add neotest

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "antoinemadec/FixCursorHold.nvim",
+      -- add adapter for neotest
+      "nvim-neotest/neotest-vim-test",
+      "haydenmeade/neotest-jest",
+      "marilari88/neotest-vitest",
+      "markemmons/neotest-deno",
+      "nvim-neotest/neotest-go",
+      "rouge8/neotest-rust",
+    },
+    opts = {
+      -- Can be a list of adapters like what neotest expects,
+      -- or a table of adapter names, mapped to adapter configs.
+      -- The adapter will then be automatically loaded with the config.
+      adapters = {
+        ["neotest-deno"] = {},
+        ["neotest-jest"] = {},
+        ["neotest-vitest"] = {},
+        ["neotest-vim-test"] = {
+          ignore_file_types = { "python", "vim", "lua" },
+        },
+        ["neotest-go"] = {},
+        ["neotest-rust"] = {},
+      },
+    },
+  },
+}
