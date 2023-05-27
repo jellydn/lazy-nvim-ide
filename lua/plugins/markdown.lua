b58fe2529fac01d4ef1b06a643ab707787fe98fd
Huynh Duc Dung <dunghd.it@gmail.com>
2023-05-27 17:52:19 +0800

    feat: add markdown preview

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "previm/previm",
+    ft = "markdown",
+    config = function()
+      -- define global for open markdonw preview, let g:previm_open_cmd = 'open -a Safari'
+      vim.g.previm_open_cmd = "open -a Arc"
+    end,
+    keys = {
+      -- add <leader>m to open markdown preview
+      {
+        "<leader>m",
+        "<cmd>PrevimOpen<cr>",
+        desc = "Markdown preview",
+      },
+    },
+  },
+}
