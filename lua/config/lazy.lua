3b58bbd99a8d59143707ad0b01e586da8b63bec3
Huynh Duc Dung <dunghd.it@gmail.com>
2023-05-29 10:58:35 +0800

    refactor: add copilot back

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,7 +16,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
+    -- { import = "lazyvim.plugins.extras.coding.copilot" },
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- { import = "lazyvim.plugins.extras.linting.eslint" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,55 @@
+return {
+  -- Disable default <tab> and <s-tab> behavior in LuaSnip
+  {
+    "L3MON4D3/LuaSnip",
+    event = "VeryLazy",
+    keys = function()
+      return {}
+    end,
+  },
+  {
+    "github/copilot.vim",
+    event = "VeryLazy",
+    config = function()
+      -- For copilot.vim
+      -- enable copilot for specific filetypes
+      vim.g.copilot_filetypes = {
+        ["TelescopePrompt"] = false,
+      }
+
+      vim.cmd([[
+        let g:copilot_assume_mapped = v:true
+      ]])
+
+      -- setup keymap
+      local keymap = vim.keymap.set
+      -- Silent keymap option
+      local opts = { silent = true }
+
+      -- Copilot
+      keymap("i", "<C-j>", "<Plug>(copilot-next)", opts)
+      keymap("i", "<C-k>", "<Plug>(copilot-previous)", opts)
+      keymap("i", "<C-l>", "<Plug>(copilot-suggest)", opts)
+    end,
+  },
+  -- add status line icon for copilot
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      local Util = require("lazyvim.util")
+      table.insert(opts.sections.lualine_x, 2, {
+        function()
+          local icon = require("lazyvim.config").icons.kinds.Copilot
+          return icon
+        end,
+        cond = function()
+          local ok, clients = pcall(vim.lsp.get_active_clients, { name = "copilot", bufnr = 0 })
+          return ok and #clients > 0
+        end,
+        color = function()
+          return Util.fg("Special")
+        end,
+      })
+    end,
+  },
+}
