1b1ae3078bd90544446fec1140eca2667b9b783c
Huynh Duc Dung <dunghd.it@gmail.com>
2023-05-31 22:20:42 +0800

    chore: enable spell check by default

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,3 +8,6 @@ vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decr
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+
+-- Enable spell check by default
+vim.o.spell = true
