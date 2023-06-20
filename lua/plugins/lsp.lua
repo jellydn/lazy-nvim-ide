175c1585925a8190a170c8a6c93b0f11dcef2ecf
Huynh Duc Dung <dunghd.it@gmail.com>
2023-06-20 19:39:30 +0800

    feat: add dim on LSP for unuse variable

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,11 +5,12 @@ return {
     config = true,
     dependencies = {
       { "nvim-tree/nvim-web-devicons" },
-      --Please make sure you install markdown and markdown_inline parser
+      -- Please make sure you install markdown and markdown_inline parser
       { "nvim-treesitter/nvim-treesitter" },
     },
   },
   {
+    -- Displaying references and definition infos upon functions like JB's IDEA.
     "VidocqH/lsp-lens.nvim",
     event = "BufRead",
     opts = {
@@ -28,5 +29,12 @@ return {
         desc = "LSP Len Toggle",
       },
     },
+  },
+  {
+    -- Dim the unused variables and functions using lsp and treesitter.
+    "narutoxy/dim.lua",
+    event = "BufRead",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "neovim/nvim-lspconfig" },
+    confg = true,
   },
 }
