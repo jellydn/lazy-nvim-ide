5298b656354c9bffbbf09ecc9b8bc1ba2427227f
Huynh Duc Dung <dunghd.it@gmail.com>
2023-06-21 21:02:17 +0800

    feat: set conceal to 0 as default

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
 
 -- Enable spell check by default
 vim.o.spell = true
+-- Set conceal level to 0
+vim.o.conceallevel = 0
