4024b6a8cf798e9eb6cf6fbab81b458da4c179e6
Huynh Duc Dung <dunghd.it@gmail.com>
2023-06-23 10:28:49 +0800

    feat: add inlay hints for typescript

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -12,6 +12,33 @@ return {
         on_attach = function(client)
           client.resolved_capabilities.document_formatting = false -- disable formatting in tsserver in favor of null-ls
         end,
+        -- inlay hints
+        settings = {
+          typescript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "none", -- 'none' | 'literals' | 'all'
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = false,
+              includeInlayVariableTypeHints = true,
+              includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+              includeInlayPropertyDeclarationTypeHints = false,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+          },
+          javascript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "none", -- 'none' | 'literals' | 'all'
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayVariableTypeHints = true,
+              includeInlayFunctionParameterTypeHints = false,
+              includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+              includeInlayPropertyDeclarationTypeHints = false,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+          },
+        },
       },
       denols = {
         root_dir = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc"),
@@ -21,20 +48,24 @@ return {
         single_file_support = false,
       },
     },
+    -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
+    -- Be aware that you also will need to properly configure your LSP server to
+    -- provide the inlay hints.
+    inlay_hints = {
+      enabled = true,
+    },
     format = {
       timeout_ms = 10000, -- 10 seconds
     },
     ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
     setup = {
-      --   tsserver = function(_, opts)
-      --     require("typescript").setup({
-      --       server = opts,
-      --       root_dir = lspconfig.util.root_pattern("package.json"),
-      --       single_file_support = false,
-      --     })
-      --     return true
-      --   end,
-      --
+      -- tsserver = function(_, opts)
+      --   -- Configure `typescript.nvim`.
+      --   require("typescript").setup({
+      --     server = opts,
+      --   })
+      --   return true
+      -- end,
       ruff_lsp = function(_, opts)
         -- Configure `ruff-lsp`.
         -- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff_lsp
