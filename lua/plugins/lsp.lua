7c85e16661c428eeb9efcea7a6634aadd4a5229a
Huynh Duc Dung <dunghd.it@gmail.com>
2023-06-28 20:42:20 +0800

    feat: add inlay hints for stable nvim 0.9.0

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,44 @@
+-- disable lsp-inlayhints if that is nightly version, will remove when 0.10.0 is stable
+local enabled_inlay_hints = true
+if vim.fn.has("nvim-0.10.0") == 1 then
+  enabled_inlay_hints = false
+end
+
 return {
   {
+    "lvimuser/lsp-inlayhints.nvim",
+    event = "LspAttach",
+    enabled = enabled_inlay_hints,
+    opts = {
+      debug_mode = true,
+    },
+    config = function(_, options)
+      vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = "LspAttach_inlayhints",
+        callback = function(args)
+          if not (args.data and args.data.client_id) then
+            return
+          end
+
+          local bufnr = args.buf
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          require("lsp-inlayhints").on_attach(client, bufnr)
+        end,
+      })
+      require("lsp-inlayhints").setup(options)
+      -- define key map for toggle inlay hints: require('lsp-inlayhints').toggle()
+      vim.api.nvim_set_keymap(
+        "n",
+        "<leader>uI",
+        "<cmd>lua require('lsp-inlayhints').toggle()<CR>",
+        { noremap = true, silent = true }
+      )
+    end,
+  },
+  {
     "glepnir/lspsaga.nvim",
-    event = "BufRead",
+    event = "LspAttach",
     config = true,
     dependencies = {
       { "nvim-tree/nvim-web-devicons" },
