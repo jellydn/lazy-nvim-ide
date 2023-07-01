a2409f64134035992207ffff53b9d50cb425e16b
Huynh Duc Dung <dunghd.it@gmail.com>
2023-07-02 00:28:27 +0800

    feat: add keymap for hardtime
    disable hardtime by default

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -5,5 +5,13 @@ return {
       -- default values is disabled the arrow keys
       disabled_keys = {},
     },
+    keys = {
+      -- disable hardtime by default, only Enable it when I need it
+      {
+        "<leader>ht",
+        "<cmd>Hardtime toggle<cr>",
+        desc = "Toggle hardtime.nvim",
+      },
+    },
   },
 }
