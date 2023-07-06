1367dfb5ce5dd605b834446bda9da6df21a899c5
Huynh Duc Dung <dunghd.it@gmail.com>
2023-07-06 22:24:32 +0800

    chore: disable hardtime by default

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,19 +1,36 @@
 return {
+  --[[
+  Recommended workflow:
+
+  - Avoid using the mouse and arrow keys if they are not at the home row of your keyboard.
+  - Use relative jump (e.g., 5j 12-) for vertical movement within the screen.
+  - Use CTRL-U CTRL-D CTRL-B CTRL-F gg G for vertical movement outside the screen.
+  - Use word-motion (w W b B e E ge gE) for short-distance horizontal movement.
+  - Use f F t T , ; 0 ^ $ for medium to long-distance horizontal movement.
+  - Use operator + motion/text-object (e.g., ci{ y5j dap) whenever possible.
+  - Use % and square bracket commands (see :h [) to jump between brackets.
+  ]]
   {
     "m4xshen/hardtime.nvim",
     opts = {
       -- default values is disabled the arrow keys
       disabled_keys = {},
-      -- allow use jk to escape
-      restricted_keys = {
-        ["h"] = { "n", "x" },
-        ["l"] = { "n", "x" },
-        ["-"] = { "n", "x" },
-        ["+"] = { "n", "x" },
-        ["<CR>"] = { "n", "x" },
-        ["<C-M>"] = { "n", "x" },
-        ["<C-N>"] = { "n", "x" },
-        ["<C-P>"] = { "n", "x" },
+      disabled_filetypes = {
+        "qf",
+        "netrw",
+        "NvimTree",
+        "lazy",
+        "mason",
+        "neo-tree",
+        "aerial",
+        "lspsagaoutline",
+      },
+    },
+    keys = {
+      {
+        "<leader>ht",
+        "<cmd>Hardtime toggle<CR>",
+        desc = "Toggle Hardtime",
       },
     },
   },
