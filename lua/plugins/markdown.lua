cf3d647b14b7381252a9c4c1e7388e17a028fac7
Huynh Duc Dung <dunghd.it@gmail.com>
2023-07-06 20:27:01 +0800

    feat: load markdown plugin on lazy event

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,12 +1,11 @@
 return {
   {
     "previm/previm",
-    cmd = "PrevimOpen",
-    ft = "markdown",
     config = function()
       -- define global for open markdonw preview, let g:previm_open_cmd = 'open -a Safari'
       vim.g.previm_open_cmd = "open -a Arc"
     end,
+    event = "VeryLazy",
     keys = {
       -- add <leader>m to open markdown preview
       {
