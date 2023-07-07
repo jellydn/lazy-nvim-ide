16c47f0718025dcb9c766dbd24a48d0fe7c7e988
Huynh Duc Dung <dunghd.it@gmail.com>
2023-07-07 22:25:55 +0800

    chore: enable neodev

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,8 +1,4 @@
 return {
-  -- disable nvim spectrum
   { "windwp/nvim-spectre", enabled = false },
-  -- disable nvim-notify
   { "rcarriga/nvim-notify", enabled = false },
-  -- { "folke/noice.nvim", enabled = false },
-  { "folke/neoconf.nvim", enabled = false },
 }
