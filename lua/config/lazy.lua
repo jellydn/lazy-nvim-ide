159a52a9758e4178114eaff277429360c62680d0
Huynh Duc Dung <dunghd.it@gmail.com>
2023-07-13 18:57:31 +0800

    fix: remove flash plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,7 +11,6 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.editor.flash" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
