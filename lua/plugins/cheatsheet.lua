e38a4423bcfc85435e03c2b4af256fa857b70cbb
Huynh Duc Dung <dunghd.it@gmail.com>
2023-07-21 15:26:24 +0800

    feat: add cheatsheet extension for telescope

---
+++ lua/plugins/cheatsheet.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "doctorfree/cheatsheet.nvim",
+    dependencies = {
+      { "nvim-telescope/telescope.nvim" },
+      { "nvim-lua/popup.nvim" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    keys = {
+      { "<leader>ch", "<cmd>Cheatsheet<cr>", desc = "Cheatsheet" },
+    },
+    config = function()
+      local ctactions = require("cheatsheet.telescope.actions")
+      require("cheatsheet").setup({
+        bundled_cheetsheets = {
+          enabled = { "default", "lua", "markdown", "regex", "netrw", "unicode" },
+          disabled = { "nerd-fonts" },
+        },
+        bundled_plugin_cheatsheets = {
+          enabled = {
+            "goto-preview",
+            "gitsigns.nvim",
+            "telescope.nvim",
+          },
+          disabled = { "gitsigns" },
+        },
+        include_only_installed_plugins = true,
+        telescope_mappings = {
+          ["<CR>"] = ctactions.select_or_fill_commandline,
+          ["<A-CR>"] = ctactions.select_or_execute,
+          ["<C-Y>"] = ctactions.copy_cheat_value,
+          ["<C-E>"] = ctactions.edit_user_cheatsheet,
+        },
+      })
+    end,
+  },
+}
