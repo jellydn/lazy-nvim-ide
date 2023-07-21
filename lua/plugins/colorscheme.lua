c5b484f980cb4d222a220a97a2899efeabae1b00
Huynh Duc Dung <dunghd.it@gmail.com>
2023-07-21 18:59:19 +0800

    feat: add borderless for telescope

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -42,6 +42,23 @@ return {
           },
         },
       },
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          -- Transparent Floating Windows
+          NormalFloat = { bg = "none" },
+          FloatBorder = { bg = "none" },
+          FloatTitle = { bg = "none" },
+          -- Borderless Telescope
+          TelescopeTitle = { fg = theme.ui.special, bold = true },
+          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+        }
+      end,
     },
   },
   {
