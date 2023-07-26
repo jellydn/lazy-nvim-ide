0b0693df120c80770651d2d8f3295181229f332e
Huynh Duc Dung <dunghd.it@gmail.com>
2023-07-26 09:08:33 +0800

    feat: add yaml lang

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
     -- { import = "lazyvim.plugins.extras.coding.copilot" },
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- { import = "lazyvim.plugins.extras.coding.yanky" },
