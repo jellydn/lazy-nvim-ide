7490ce64fe0c24f3d3bb90186493014e22b1c457
Huynh Duc Dung <dunghd.it@gmail.com>
2023-08-14 20:56:06 +0800

    fix: turn off python3 provider

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,13 +3,13 @@
 
 -- Enable spell check by default unless in vscode
 if not vim.g.vscode then
-	vim.o.spell = true
+  vim.o.spell = true
 end
 -- Set conceal level to 0
 vim.o.conceallevel = 0
 
 -- Disable providers
-vim.g.loaded_python_provider = 0
+vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_node_provider = 0
