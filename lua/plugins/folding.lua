0eea4dbdab798dfe8140459ab525a96ed5ba6beb
Huynh Duc Dung <dunghd.it@gmail.com>
2023-10-04 01:09:12 +0800

    fix: simplify the folding setup

--- lua/plugins/folding.lua
+++ lua/plugins/folding.lua
@@ -4,20 +4,6 @@ return {
     "kevinhwang91/nvim-ufo",
     dependencies = {
       "kevinhwang91/promise-async",
-      {
-        "luukvbaal/statuscol.nvim",
-        config = function()
-          local builtin = require("statuscol.builtin")
-          require("statuscol").setup({
-            relculright = true,
-            segments = {
-              { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-              { text = { "%s" }, click = "v:lua.ScSa" },
-              { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
-            },
-          })
-        end,
-      },
     },
     event = "BufReadPost",
     opts = {
@@ -25,20 +11,6 @@ return {
         return { "treesitter", "indent" }
       end,
     },
-    init = function()
-      vim.o.foldcolumn = "1" -- '0' is not bad
-      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-      vim.o.foldlevelstart = 99
-      vim.o.foldenable = true
-      vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-
-      vim.keymap.set("n", "zR", function()
-        require("ufo").openAllFolds()
-      end)
-      vim.keymap.set("n", "zM", function()
-        require("ufo").closeAllFolds()
-      end)
-    end,
   },
   -- Folding preview, by default h and l keys are used.
   -- On first press of h key, when cursor is on a closed fold, the preview will be shown.
