468db53fdc3f12fb6d5d01ca0d9412a1a1994a52
Huynh Duc Dung <dunghd.it@gmail.com>
2023-10-10 21:17:25 +0800

    refactor: add tabnine cmp

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.coding.codeium" },
     -- { import = "lazyvim.plugins.extras.coding.copilot" },
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- { import = "lazyvim.plugins.extras.coding.yanky" },
