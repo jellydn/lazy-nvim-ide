b6b101f64af0c144c5a422441688938a4f86cbc8
Huynh Duc Dung <dunghd.it@gmail.com>
2023-10-12 07:24:28 +0800

    chore: remove fzf native as installed with LazyVim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -89,16 +89,5 @@ return {
         desc = "Find All Files (including hidden)",
       },
     },
-    -- add telescope-fzf-native
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      cond = function()
-        return vim.fn.executable("make") == 1
-      end,
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
-    },
   },
 }
