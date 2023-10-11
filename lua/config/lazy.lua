14e753cc8361fbebf49d9e80df737c6ff6f279ee
Huynh Duc Dung <dunghd.it@gmail.com>
2023-10-12 07:23:40 +0800

    feat: enable copilot extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,7 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.coding.codeium" },
-    -- { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- { import = "lazyvim.plugins.extras.coding.yanky" },
     -- { import = "lazyvim.plugins.extras.vscode" },
