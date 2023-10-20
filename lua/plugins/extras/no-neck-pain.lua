da7fff68083d6fa3ad7feaee2a1f6a8843ba8e9e
Huynh Duc Dung <dunghd.it@gmail.com>
2023-10-20 20:38:49 +0800

    chore: add no neck pain plugin

---
+++ lua/plugins/extras/no-neck-pain.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "shortcuts/no-neck-pain.nvim",
+    cmd = "NoNeckPain",
+    keys = {
+      {
+        "<leader>cZ",
+        "<cmd>NoNeckPain<cr>",
+        desc = "No Neck Pain",
+      },
+    },
+  },
+}
