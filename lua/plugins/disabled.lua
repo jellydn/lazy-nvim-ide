57b5855ded42529880e13505c022a122fbf15843
Huynh Duc Dung <dunghd.it@gmail.com>
2023-10-30 14:29:43 +0800

    chore: disable conform plugin

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
   { "rcarriga/nvim-notify", enabled = false },
   { "folke/neoconf.nvim", enabled = false },
+  -- Use none-ls for until migration to "stevearc/conform.nvim",
+  { "stevearc/conform.nvim", enabled = false },
 }
