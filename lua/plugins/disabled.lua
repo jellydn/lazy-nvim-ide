edec326284a2585b72e17a1f73ae127d956b8d03
Huynh Duc Dung <dunghd.it@gmail.com>
2023-11-03 19:28:02 +0800

    chore: disable nvim-lint

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "folke/neoconf.nvim", enabled = false },
   -- Use none-ls for until migration to "stevearc/conform.nvim",
   { "stevearc/conform.nvim", enabled = false },
+  { "mfussenegger/nvim-lint", enabled = false },
 }
