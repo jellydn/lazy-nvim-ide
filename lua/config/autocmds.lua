ebb334b3114e4a23abf22f3e3c32cc5a967c9983
Huynh Duc Dung <dunghd.it@gmail.com>
2023-11-06 19:56:07 +0800

    feat: add autocommand for .env file

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+local function augroup(name)
+  return vim.api.nvim_create_augroup("my_lazyvim_" .. name, { clear = true })
+end
+
+-- Set filetype for .env and .env.* files
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  group = augroup("env_filetype"),
+  pattern = { "*.env", ".env.*" },
+  callback = function()
+    vim.opt_local.filetype = "sh"
+  end,
+})
