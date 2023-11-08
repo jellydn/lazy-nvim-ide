3dc66b65ac2109f3d5154fad133ec3019601c478
Huynh Duc Dung <dunghd.it@gmail.com>
2023-11-08 19:25:20 +0800

    feat: add run to entry command for hurl

--- lua/plugins/extras/hurl.lua
+++ lua/plugins/extras/hurl.lua
@@ -3,7 +3,7 @@ return {
     "jellydn/hurl.nvim",
     ft = "hurl",
     dependencies = { "MunifTanjim/nui.nvim" },
-    cmd = { "HurlRunner", "HurlRunnerAt" },
+    cmd = { "HurlRunner", "HurlRunnerAt", "HurlRunnerToEntry" },
     opts = {
       mode = "popup",
       debug = false, -- Enable debug for troubleshooting
@@ -12,6 +12,7 @@ return {
       -- Run API request
       { "<leader>A", "<cmd>HurlRunner<CR>", desc = "Run All requests" },
       { "<leader>a", "<cmd>HurlRunnerAt<CR>", desc = "Run Api request" },
+      { "<leader>te", "<cmd>HurlRunnerToEntry<CR>", desc = "Run Api request to entry" },
       -- Run Hurl request in visual mode
       { "<leader>h", ":HurlRunner<CR>", desc = "Hurl Runner", mode = "v" },
     },
