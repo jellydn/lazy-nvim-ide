5f1d1c2b1777c999dd6a93ce68634c147bf3c4cf
Huynh Duc Dung <dunghd.it@gmail.com>
2023-11-10 20:20:05 +0800

    feat: add new command for toggle hurl mode

--- lua/plugins/extras/hurl.lua
+++ lua/plugins/extras/hurl.lua
@@ -3,7 +3,6 @@ return {
     "jellydn/hurl.nvim",
     ft = "hurl",
     dependencies = { "MunifTanjim/nui.nvim" },
-    cmd = { "HurlRunner", "HurlRunnerAt", "HurlRunnerToEntry" },
     opts = {
       mode = "popup",
       debug = false, -- Enable debug for troubleshooting
@@ -13,6 +12,7 @@ return {
       { "<leader>A", "<cmd>HurlRunner<CR>", desc = "Run All requests" },
       { "<leader>a", "<cmd>HurlRunnerAt<CR>", desc = "Run Api request" },
       { "<leader>te", "<cmd>HurlRunnerToEntry<CR>", desc = "Run Api request to entry" },
+      { "<leader>tm", "<cmd>HurlToggleMode<CR>", desc = "Hurl Toggle Mode" },
       -- Run Hurl request in visual mode
       { "<leader>h", ":HurlRunner<CR>", desc = "Hurl Runner", mode = "v" },
     },
