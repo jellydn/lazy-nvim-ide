dc791f8310fda19627f395aaf4df3a6ea5079bdf
Huynh Duc Dung <dunghd.it@gmail.com>
2023-11-16 12:22:35 +0800

    chore: add width for neck pain plugin

--- lua/plugins/extras/no-neck-pain.lua
+++ lua/plugins/extras/no-neck-pain.lua
@@ -1,10 +1,14 @@
+-- Either use code zen or no neck pain plugin. It's using the same keybinding.
 return {
   {
     "shortcuts/no-neck-pain.nvim",
     cmd = "NoNeckPain",
+    opts = {
+      width = 120,
+    },
     keys = {
       {
-        "<leader>cZ",
+        "<leader>cz",
         "<cmd>NoNeckPain<cr>",
         desc = "No Neck Pain",
       },
