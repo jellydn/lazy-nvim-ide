d3209ce30f1bf244f83274c7693d46c17a69b16f
Huynh Duc Dung <dunghd.it@gmail.com>
2023-11-18 16:20:38 +0800

    chore: add keymap for run hurl in verbose mode

--- lua/plugins/extras/hurl.lua
+++ lua/plugins/extras/hurl.lua
@@ -13,6 +13,7 @@ return {
       { "<leader>a", "<cmd>HurlRunnerAt<CR>", desc = "Run Api request" },
       { "<leader>te", "<cmd>HurlRunnerToEntry<CR>", desc = "Run Api request to entry" },
       { "<leader>tm", "<cmd>HurlToggleMode<CR>", desc = "Hurl Toggle Mode" },
+      { "<leader>tv", "<cmd>HurlVerbose<CR>", desc = "Run Api in verbose mode" },
       -- Run Hurl request in visual mode
       { "<leader>h", ":HurlRunner<CR>", desc = "Hurl Runner", mode = "v" },
     },
