ca6ff42d135f042f8b354874b4003e94ded19a6e
Huynh Duc Dung <dunghd.it@gmail.com>
2023-12-01 14:23:13 +0800

    chore: enable cspell by default

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -6,13 +6,6 @@ return {
         markdown = { "markdownlint" },
         ["*"] = { "cspell", "codespell" },
       },
-      linters = {
-        cspell = {
-          condition = function(ctx)
-            return vim.fs.find({ "cspell.json" }, { path = ctx.filename, upward = true })[1]
-          end,
-        },
-      },
     },
   },
 }
