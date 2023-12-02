33060af088fc5bcba0561550c3bae167dff09e48
Huynh Duc Dung <dunghd.it@gmail.com>
2023-12-02 13:22:29 +0800

    chore: make hurl.nvim to 40% width with edgy

--- lua/plugins/edgy.lua
+++ lua/plugins/edgy.lua
@@ -2,53 +2,12 @@ return {
   { import = "lazyvim.plugins.extras.ui.edgy" },
   {
     "folke/edgy.nvim",
-    keys = {
-      {
-        "<leader>uE",
-        function()
-          require("edgy").select()
-        end,
-        desc = "Edgy Select Window",
-      },
-      {
-        "<leader>ue",
-        function()
-          require("edgy").toggle()
-        end,
-        desc = "Edgy Toggle",
-      },
-    },
-    event = "VeryLazy",
-    init = function()
-      vim.opt.laststatus = 3
-      vim.opt.splitkeep = "screen"
-    end,
     opts = {
       wo = {
         spell = false,
       },
       animate = {
         enabled = false,
-      },
-      bottom = {
-        {
-          ft = "lazyterm",
-          title = "LazyTerm",
-          size = { height = 0.4 },
-          filter = function(buf)
-            return not vim.b[buf].lazyterm_cmd
-          end,
-        },
-        "Trouble",
-        { ft = "qf", title = "QuickFix" },
-        {
-          ft = "help",
-          size = { height = 20 },
-          -- only show help buffers
-          filter = function(buf)
-            return vim.bo[buf].buftype == "help"
-          end,
-        },
       },
       left = {
         "neo-tree",
@@ -61,7 +20,7 @@ return {
           ft = "Outline",
           open = "SymbolsOutlineOpen",
         },
-        { title = "hurl.nvim", ft = "hurl-nvim" },
+        { title = "hurl.nvim", ft = "hurl-nvim", size = { 0.4 } },
         "aerial",
         "lspsagaoutline",
       },
