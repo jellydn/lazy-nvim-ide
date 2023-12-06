75f753f60b27dd1c8d9d681657c0221740233324
Huynh Duc Dung <dunghd.it@gmail.com>
2023-12-06 10:41:46 +0800

    refactor!: remove lsp lens in favors of symbol-usage

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -99,23 +99,64 @@ return {
     },
   },
   {
-    -- Displaying references and definition infos upon functions
-    "VidocqH/lsp-lens.nvim",
-    event = "BufRead",
-    opts = {
-      sections = { -- Enable / Disable specific request, formatter example looks 'Format Requests'
-        definition = false,
-        implements = false,
-        git_authors = false,
-      },
-    },
-    keys = {
-      {
-        -- LspLensToggle
-        "<leader>uL",
-        "<cmd>LspLensToggle<CR>",
-        desc = "LSP Len Toggle",
-      },
-    },
+    "Wansmer/symbol-usage.nvim",
+    event = is_stable_version and "LspAttach" or "BufReadPre",
+    opts = function()
+      local function h(name)
+        return vim.api.nvim_get_hl(0, { name = name })
+      end
+
+      -- hl-groups can have any name
+      vim.api.nvim_set_hl(0, "SymbolUsageRounding", { fg = h("CursorLine").bg, italic = true })
+      vim.api.nvim_set_hl(0, "SymbolUsageContent", { bg = h("CursorLine").bg, fg = h("Comment").fg, italic = true })
+      vim.api.nvim_set_hl(0, "SymbolUsageRef", { fg = h("Function").fg, bg = h("CursorLine").bg, italic = true })
+      vim.api.nvim_set_hl(0, "SymbolUsageDef", { fg = h("Type").fg, bg = h("CursorLine").bg, italic = true })
+      vim.api.nvim_set_hl(0, "SymbolUsageImpl", { fg = h("@keyword").fg, bg = h("CursorLine").bg, italic = true })
+
+      local function text_format(symbol)
+        local res = {}
+
+        local round_start = { "", "SymbolUsageRounding" }
+        local round_end = { "", "SymbolUsageRounding" }
+
+        if symbol.references then
+          local usage = symbol.references <= 1 and "usage" or "usages"
+          local num = symbol.references == 0 and "no" or symbol.references
+          table.insert(res, round_start)
+          table.insert(res, { "󰌹 ", "SymbolUsageRef" })
+          table.insert(res, { ("%s %s"):format(num, usage), "SymbolUsageContent" })
+          table.insert(res, round_end)
+        end
+
+        if symbol.definition then
+          if #res > 0 then
+            table.insert(res, { " ", "NonText" })
+          end
+          table.insert(res, round_start)
+          table.insert(res, { "󰳽 ", "SymbolUsageDef" })
+          table.insert(res, { symbol.definition .. " defs", "SymbolUsageContent" })
+          table.insert(res, round_end)
+        end
+
+        if symbol.implementation then
+          if #res > 0 then
+            table.insert(res, { " ", "NonText" })
+          end
+          table.insert(res, round_start)
+          table.insert(res, { "󰡱 ", "SymbolUsageImpl" })
+          table.insert(res, { symbol.implementation .. " impls", "SymbolUsageContent" })
+          table.insert(res, round_end)
+        end
+
+        return res
+      end
+
+      return {
+        references = { enabled = true, include_declaration = false },
+        definition = { enabled = false },
+        implementation = { enabled = false },
+        text_format = text_format,
+      }
+    end,
   },
 }
