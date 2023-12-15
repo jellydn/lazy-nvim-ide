5128bc3260f463ff32e060127da6be1be40fe345
Huynh Duc Dung <dunghd.it@gmail.com>
2023-12-15 15:45:36 +0800

    feat: add copilot chat plugin

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,8 +8,9 @@ end
 -- Set conceal level to 0
 vim.o.conceallevel = 0
 
+-- Enable python3 provider back for CopilotChat.nvim
+-- vim.g.loaded_python3_provider = 0
 -- Disable providers
-vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_node_provider = 0
