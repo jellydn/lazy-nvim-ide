fa2c3e636d5f16df5248730f88e2a8e01739ef41
Huynh Duc Dung <dunghd.it@gmail.com>
2024-01-13 14:50:33 +0800

    chore: add custom prompt for gen.nvim

--- lua/plugins/extras/local-llm.lua
+++ lua/plugins/extras/local-llm.lua
@@ -1,12 +1,16 @@
+local IS_DEV = false
+
 return {
   -- Easy to use, need to have a model downloaded
   {
-    "David-Kunz/gen.nvim",
+    "jellydn/gen.nvim",
+    dir = IS_DEV and "~/Projects/research/gen.nvim" or nil,
     opts = {
       model = "mistral", -- The default model to use. If you don't have, run `ollama pull mistral` on your terminal.
       display_mode = "split", -- The display mode. Can be "float" or "split".
       show_model = true, -- Displays which model you are using at the beginning of your chat session.
       debug = false, -- Prints errors and the command which is run.
+      show_prompt = false, -- Shows the prompt text.
     },
     event = "VeryLazy",
     keys = {
@@ -18,6 +22,17 @@ return {
         desc = "Gen - Select AI Model",
       },
     },
+    config = function(_, opts)
+      local gen = require("gen")
+      gen.setup(opts)
+      gen.prompts["Elaborate_Text"] = {
+        prompt = "Elaborate the following text:\n$text",
+      }
+      gen.prompts["Fix_Code"] = {
+        prompt = "Fix the following code. Only output the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
+        extract = "```$filetype\n(.-)```",
+      }
+    end,
   },
   -- A bit lower level than gen.nvim, works out of the box with local models like ollama, llama.cpp
   {
