8f6d006c7f770e177734209d215e552067912943
Huynh Duc Dung <dunghd.it@gmail.com>
2024-01-14 14:00:31 +0800

    chore: remove model.nvim
    chore: update lock

--- lua/plugins/extras/gen-nvim.lua
+++ lua/plugins/extras/gen-nvim.lua
@@ -8,9 +8,9 @@ return {
     opts = {
       model = "mistral", -- The default model to use. If you don't have, run `ollama pull mistral` on your terminal.
       display_mode = "split", -- The display mode. Can be "float" or "split".
+      show_prompt = true, -- Shows the Prompt submitted to Ollama.
       show_model = true, -- Displays which model you are using at the beginning of your chat session.
       debug = false, -- Prints errors and the command which is run.
-      show_prompt = false, -- Shows the prompt text.
     },
     event = "VeryLazy",
     keys = {
@@ -33,37 +33,5 @@ return {
         extract = "```$filetype\n(.-)```",
       }
     end,
-  },
-  -- A bit lower level than gen.nvim, works out of the box with local models like ollama, llama.cpp
-  {
-    "gsuuon/model.nvim",
-    cmd = { "M", "Model", "Mchat" },
-    init = function()
-      vim.filetype.add({
-        extension = {
-          mchat = "mchat",
-        },
-      })
-    end,
-    enabled = false, -- NOTE: Will revise this later when https://github.com/gsuuon/model.nvim/discussions/9 is done
-    config = function()
-      local ollama = require("model.providers.ollama")
-      require("model").setup({
-        providers = {
-          ["ollama:starling"] = {
-            provider = ollama,
-            params = {
-              model = "starling-lm",
-            },
-            builder = function(input)
-              return {
-                prompt = "GPT4 Correct User: " .. input .. "<|end_of_turn|>GPT4 Correct Assistant: ",
-              }
-            end,
-          },
-        },
-      })
-    end,
-    ft = "mchat",
   },
 }
