3a29ea76520c98c15023f0f9aae57ac3866d9d08
Huynh Duc Dung <dunghd.it@gmail.com>
2024-01-20 22:28:57 +0800

    chore: add more commands for wording with CopilotChat

--- lua/plugins/extras/copilot-chat.lua
+++ lua/plugins/extras/copilot-chat.lua
@@ -1,5 +1,18 @@
 local IS_DEV = false
 
+local prompts = {
+  -- Code related prompts
+  Explain = "Please explain how the following code works.",
+  Review = "Please review the following code and provide suggestions for improvement.",
+  Tests = "Please explain how the selected code works, then generate unit tests for it.",
+  Refactor = "Please refactor the following code to improve its clarity and readability.",
+  -- Text related prompts
+  Summarize = "Please summarize the following text.",
+  Spelling = "Please correct any grammar and spelling errors in the following text.",
+  Wording = "Please improve the grammar and wording of the following text.",
+  Concise = "Please rewrite the following text to make it more concise.",
+}
+
 return {
   -- Import the copilot plugin
   { import = "lazyvim.plugins.extras.coding.copilot" },
@@ -8,12 +21,7 @@ return {
     "jellydn/CopilotChat.nvim",
     opts = {
       mode = "split",
-      prompts = {
-        Explain = "Explain how it works.",
-        Review = "Review the following code and provide concise suggestions.",
-        Tests = "Briefly explain how the selected code works, then generate unit tests.",
-        Refactor = "Refactor the code to improve clarity and readability.",
-      },
+      prompts = prompts,
     },
     build = function()
       vim.defer_fn(function()
@@ -27,6 +35,10 @@ return {
       { "<leader>cct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
       { "<leader>ccr", "<cmd>CopilotChatReview<cr>", desc = "CopilotChat - Review code" },
       { "<leader>ccR", "<cmd>CopilotChatRefactor<cr>", desc = "CopilotChat - Refactor code" },
+      { "<leader>ccs", "<cmd>CopilotChatSummarize<cr>", desc = "CopilotChat - Summarize text" },
+      { "<leader>ccS", "<cmd>CopilotChatSpelling<cr>", desc = "CopilotChat - Correct spelling" },
+      { "<leader>ccw", "<cmd>CopilotChatWording<cr>", desc = "CopilotChat - Improve wording" },
+      { "<leader>ccc", "<cmd>CopilotChatConcise<cr>", desc = "CopilotChat - Make text concise" },
     },
   },
 }
