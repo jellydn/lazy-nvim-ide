165494781f2e144b9ea099c59910b7a4f3c291fe
Huynh Duc Dung <dunghd.it@gmail.com>
2024-01-20 21:55:11 +0800

    feat: add more commands for CopilotChat

--- lua/plugins/extras/copilot-chat.lua
+++ lua/plugins/extras/copilot-chat.lua
@@ -8,6 +8,12 @@ return {
     "jellydn/CopilotChat.nvim",
     opts = {
       mode = "split",
+      prompts = {
+        Explain = "Explain how it works.",
+        Review = "Review the following code and provide concise suggestions.",
+        Tests = "Briefly explain how the selected code works, then generate unit tests.",
+        Refactor = "Refactor the code to improve clarity and readability.",
+      },
     },
     build = function()
       vim.defer_fn(function()
@@ -19,6 +25,8 @@ return {
     keys = {
       { "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
       { "<leader>cct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
+      { "<leader>ccr", "<cmd>CopilotChatReview<cr>", desc = "CopilotChat - Review code" },
+      { "<leader>ccR", "<cmd>CopilotChatRefactor<cr>", desc = "CopilotChat - Refactor code" },
     },
   },
 }
