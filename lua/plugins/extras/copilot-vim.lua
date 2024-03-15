ffa9b27a734f5214ad5d690896cd382085429f3e
Huynh Duc Dung <dunghd.it@gmail.com>
2024-03-15 21:06:51 +0800

    feat(copilot-vim): enhance nvim-cmp and copilot.vim configurations

--- lua/plugins/extras/copilot-vim.lua
+++ lua/plugins/extras/copilot-vim.lua
@@ -8,6 +8,16 @@ return {
       return {}
     end,
   },
+  -- Setup nvim-cmp
+  {
+    "nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      -- Disable ghost text for nvim-cmp, use copilot suggestion instead
+      opts.experimental.ghost_text = false
+    end,
+  },
+  -- Setup copilot.vim
   {
     "github/copilot.vim",
     event = "VeryLazy",
@@ -18,15 +28,29 @@ return {
         ["TelescopePrompt"] = false,
       }
 
-      vim.cmd([[
-        let g:copilot_assume_mapped = v:true
-      ]])
+      -- Set to true to assume that copilot is already mapped
+      vim.g.copilot_assume_mapped = true
+      -- Set workspace folders
+      vim.g.copilot_workspace_folders = "~/Projects"
 
+      -- Setup keymaps
       local keymap = vim.keymap.set
       local opts = { silent = true }
+
+      -- Set <C-y> to accept copilot suggestion
+      vim.g.copilot_no_tab_map = true
+      keymap("i", "<C-y>", 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
+
+      -- Set <C-i> to accept line
+      keymap("i", "<C-i>", "<Plug>(copilot-accept-line)", opts)
+
+      -- Set <C-j> to next suggestion, <C-k> to previous suggestion, <C-l> to suggest
       keymap("i", "<C-j>", "<Plug>(copilot-next)", opts)
       keymap("i", "<C-k>", "<Plug>(copilot-previous)", opts)
       keymap("i", "<C-l>", "<Plug>(copilot-suggest)", opts)
+
+      -- Set <C-d> to dismiss suggestion
+      keymap("i", "<C-d>", "<Plug>(copilot-dismiss)", opts)
     end,
   },
   -- Add status line icon for copilot
