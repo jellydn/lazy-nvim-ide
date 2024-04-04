5208569e08ad8bcbc388251ccc3190e426b865dc
Huynh Duc Dung <dunghd.it@gmail.com>
2024-04-05 00:05:23 +0800

    feat(overseer): add overseer plugin configuration

---
+++ lua/plugins/extras/overseer.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "stevearc/overseer.nvim",
+    dependencies = {
+      "akinsho/toggleterm.nvim",
+    },
+    opts = {
+      dap = false,
+    },
+    keys = {
+      {
+        "<leader>rt",
+        ":OverseerRun<CR>",
+        desc = "Overseer - Run Task",
+      },
+      -- Quick action
+      {
+        "<leader>rq",
+        ":OverseerQuickAction<CR>",
+        desc = "Overseer - Quick Action",
+      },
+      -- Rerun last command
+      {
+        "<leader>rr",
+        function()
+          local overseer = require("overseer")
+          local tasks = overseer.list_tasks({ recent_first = true })
+          if vim.tbl_isempty(tasks) then
+            vim.notify("No tasks found", vim.log.levels.WARN)
+          else
+            overseer.run_action(tasks[1], "restart")
+          end
+        end,
+        desc = "Overseer - Rerun Last Task",
+      },
+      -- Toggle
+      {
+        "<leader>ro",
+        ":OverseerToggle bottom<CR>",
+        desc = "Overseer - Toggle at bottom",
+      },
+    },
+  },
+}
