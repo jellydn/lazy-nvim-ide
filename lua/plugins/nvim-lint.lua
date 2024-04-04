0b5856bc0f8ad6de2a14069b0cd095cd0eeec8f8
Huynh Duc Dung <dunghd.it@gmail.com>
2024-04-05 00:05:39 +0800

    refactor(nvim-lint): disable markdownlint

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -3,7 +3,7 @@ return {
     "mfussenegger/nvim-lint",
     opts = {
       linters_by_ft = {
-        markdown = { "markdownlint" },
+        -- markdown = { "markdownlint" },
         -- Need to install golangci-lint
         -- e.g: brew install golangci-lint
         go = { "golangcilint" },
