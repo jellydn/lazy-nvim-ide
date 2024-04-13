712f9ec1c1a9ae5946c2139b6c41d35319577750
Huynh Duc Dung <dunghd.it@gmail.com>
2024-04-13 23:13:34 +0800

    refactor(lsp-config): remove tsserver and related plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,8 +15,11 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-    { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "plugins.extras.tsserver" },
+    -- { import = "plugins.extras.vtsls" }, -- Typescript Server alternative
+    -- { import = "plugins.extras.typescript-tools" }, -- Typescript Server alternative
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- { import = "lazyvim.plugins.extras.coding.yanky" },
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,24 +3,6 @@ local Lsp = require("utils.lsp")
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
-    -- Typescript formatter
-    {
-      "dmmulroy/ts-error-translator.nvim",
-      ft = "javascript,typescript,typescriptreact,svelte",
-    },
-    {
-      "marilari88/twoslash-queries.nvim",
-      ft = "javascript,typescript,typescriptreact,svelte",
-      opts = {
-        is_enabled = false, -- Use :TwoslashQueriesEnable to enable
-        multi_line = true, -- to print types in multi line mode
-        highlight = "Type", -- to set up a highlight group for the virtual text
-      },
-      keys = {
-        { "<leader>dt", ":TwoslashQueriesEnable<cr>", desc = "Enable twoslash queries" },
-        { "<leader>dd", ":TwoslashQueriesInspect<cr>", desc = "Inspect twoslash queries" },
-      },
-    },
     -- Php
     {
       "gbprod/phpactor.nvim",
@@ -50,106 +32,6 @@ return {
   ---@class PluginLspOpts
   opts = {
     servers = {
-      tsserver = {
-        root_dir = require("lspconfig").util.root_pattern("package.json", "tsconfig.json"),
-        single_file_support = false,
-        handlers = {
-          -- format error code with better error message
-          ["textDocument/publishDiagnostics"] = function(err, result, ctx, config)
-            require("ts-error-translator").translate_diagnostics(err, result, ctx, config)
-            vim.lsp.diagnostic.on_publish_diagnostics(err, result, ctx, config)
-          end,
-        },
-        -- add keymap
-        keys = {
-          {
-            "<leader>co",
-            function()
-              vim.lsp.buf.code_action({
-                apply = true,
-                context = {
-                  only = { "source.organizeImports" },
-                  diagnostics = {},
-                },
-              })
-            end,
-            desc = "Organize Imports",
-          },
-          {
-            "<leader>cR",
-            function()
-              vim.lsp.buf.code_action({
-                apply = true,
-                context = {
-                  only = { "source.removeUnused" },
-                  diagnostics = {},
-                },
-              })
-            end,
-            desc = "Remove Unused Imports",
-          },
-        },
-        -- inlay hints & code lens, refer to https://github.com/typescript-language-server/typescript-language-server/blob/master/docs/configuration.md/#workspacedidchangeconfiguration
-        settings = {
-          typescript = {
-            -- Inlay Hints preferences
-            inlayHints = {
-              -- You can set this to 'all' or 'literals' to enable more hints
-              includeInlayParameterNameHints = "literals", -- 'none' | 'literals' | 'all'
-              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-              includeInlayFunctionParameterTypeHints = false,
-              includeInlayVariableTypeHints = false,
-              includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-              includeInlayPropertyDeclarationTypeHints = false,
-              includeInlayFunctionLikeReturnTypeHints = true,
-              includeInlayEnumMemberValueHints = true,
-            },
-            -- Code Lens preferences
-            implementationsCodeLens = {
-              enabled = true,
-            },
-            referencesCodeLens = {
-              enabled = true,
-              showOnAllFunctions = true,
-            },
-            format = {
-              indentSize = vim.o.shiftwidth,
-              convertTabsToSpaces = vim.o.expandtab,
-              tabSize = vim.o.tabstop,
-            },
-          },
-          javascript = {
-            -- Inlay Hints preferences
-            inlayHints = {
-              -- You can set this to 'all' or 'literals' to enable more hints
-              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-              includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all'
-              includeInlayVariableTypeHints = true,
-              includeInlayFunctionParameterTypeHints = true,
-              includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-              includeInlayPropertyDeclarationTypeHints = true,
-              includeInlayFunctionLikeReturnTypeHints = true,
-              includeInlayEnumMemberValueHints = true,
-            },
-            -- Code Lens preferences
-            implementationsCodeLens = {
-              enabled = true,
-            },
-            referencesCodeLens = {
-              enabled = true,
-              showOnAllFunctions = true,
-            },
-            format = {
-              indentSize = vim.o.shiftwidth,
-              convertTabsToSpaces = vim.o.expandtab,
-              tabSize = vim.o.tabstop,
-            },
-          },
-          completions = {
-            completeFunctionCalls = true,
-          },
-        },
-      },
       denols = {
         root_dir = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc", "deno.lock"),
       },
@@ -180,20 +62,6 @@ return {
     format = {
       timeout_ms = 10000, -- 10 seconds
     },
-    setup = {
-      tsserver = function()
-        -- Disable tsserver if denols is present
-        if Lsp.deno_config_exist() then
-          return true
-        end
-
-        require("lazyvim.util").lsp.on_attach(function(client, bufnr)
-          if client.name == "tsserver" then
-            -- Attach twoslash queries
-            require("twoslash-queries").attach(client, bufnr)
-          end
-        end)
-      end,
-    },
+    setup = {},
   },
 }
