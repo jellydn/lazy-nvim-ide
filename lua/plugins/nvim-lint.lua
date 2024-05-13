4c26f5ab31a7958c53ff433d8cfe86b5d6d45198
Huynh Duc Dung <dunghd.it@gmail.com>
2024-05-13 20:57:08 +0800

    feat(nvim-lint): add oxlint support for JS and TS files

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -9,7 +9,29 @@ return {
         go = { "golangcilint" },
         python = { "ruff" },
         ["*"] = { "cspell", "codespell" },
+        javascript = { "oxlint" },
+        typescript = { "oxlint" },
+        javascriptreact = { "oxlint" },
+        typescriptreact = { "oxlint" },
       },
     },
+    init = function()
+      -- Register oxlint
+      require("lint").linters.oxlint = {
+        name = "oxlint",
+        -- Refer to setup https://oxc-project.github.io/docs/contribute/development.html#cargo-binstall
+        -- Or use the following command to install
+        -- cargo install --features allocator --git https://github.com/oxc-project/oxc oxc_cli
+        cmd = "oxlint",
+        stdin = false,
+        args = { "--format", "unix" },
+        stream = "stdout",
+        ignore_exitcode = true,
+        parser = require("lint.parser").from_errorformat("%f:%l:%c: %m", {
+          source = "oxlint",
+          severity = vim.diagnostic.severity.WARN,
+        }),
+      }
+    end,
   },
 }
