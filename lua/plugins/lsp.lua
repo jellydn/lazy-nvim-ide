8e293d63db93c4d5992a32a4021fcd9edd0b9ea0
Huynh Duc Dung <dunghd.it@gmail.com>
2024-05-17 21:15:01 +0700

    feat(lsp): remove conditional logic for lsp-inlayhints and symbol-usage

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,44 +1,7 @@
--- disable lsp-inlayhints and lsp lens if that is nightly version, will remove when 0.10.0 is stable
-local is_stable_version = true
-if vim.fn.has("nvim-0.10.0") == 1 then
-  is_stable_version = false
-end
-
 return {
   {
-    "lvimuser/lsp-inlayhints.nvim",
-    ft = { "javascript", "javascriptreact", "json", "jsonc", "typescript", "typescriptreact", "svelte", "go" },
-    enabled = is_stable_version,
-    opts = {
-      debug_mode = true,
-    },
-    config = function(_, options)
-      vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = "LspAttach_inlayhints",
-        callback = function(args)
-          if not (args.data and args.data.client_id) then
-            return
-          end
-
-          local bufnr = args.buf
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          require("lsp-inlayhints").on_attach(client, bufnr)
-        end,
-      })
-      require("lsp-inlayhints").setup(options)
-      -- define key map for toggle inlay hints: require('lsp-inlayhints').toggle()
-      vim.api.nvim_set_keymap(
-        "n",
-        "<leader>uh",
-        "<cmd>lua require('lsp-inlayhints').toggle()<CR>",
-        { noremap = true, silent = true, desc = "Toggle Inlay Hints" }
-      )
-    end,
-  },
-  {
     "Wansmer/symbol-usage.nvim",
-    event = is_stable_version and "LspAttach" or "BufReadPre",
+    event = "LspAttach",
     opts = {
       vt_position = "end_of_line",
       text_format = function(symbol)
