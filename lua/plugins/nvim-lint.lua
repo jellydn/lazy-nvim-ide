3a567bbbb5065bfb005c5dfd9ac295137278e25b
Huynh Duc Dung <dunghd.it@gmail.com>
2024-05-24 22:00:08 +0800

    feat(nvim-lint): remove oxlint linter setup

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -15,23 +15,5 @@ return {
         typescriptreact = { "oxlint" },
       },
     },
-    init = function()
-      -- Register oxlint
-      require("lint").linters.oxlint = {
-        name = "oxlint",
-        -- Refer to setup https://oxc-project.github.io/docs/contribute/development.html#cargo-binstall
-        -- Or use the following command to install
-        -- cargo install --features allocator --git https://github.com/oxc-project/oxc oxc_cli
-        cmd = "oxlint",
-        stdin = false,
-        args = { "--format", "unix" },
-        stream = "stdout",
-        ignore_exitcode = true,
-        parser = require("lint.parser").from_errorformat("%f:%l:%c: %m", {
-          source = "oxlint",
-          severity = vim.diagnostic.severity.WARN,
-        }),
-      }
-    end,
   },
 }
