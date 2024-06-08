a1ce18f757265382ecd78a19cc28aff89e9ca95e
Huynh Duc Dung <dunghd.it@gmail.com>
2024-06-08 17:24:08 +0800

    feat(vtsls.lua): Add new keybindings and disable tsserver

--- lua/plugins/extras/vtsls.lua
+++ lua/plugins/extras/vtsls.lua
@@ -32,6 +32,9 @@ return {
     },
     opts = {
       servers = {
+        tsserver = {
+          enabled = false,
+        },
         -- ---@type lspconfig.options.vtsls
         vtsls = {
           -- add keymap
@@ -40,6 +43,48 @@ return {
               "<leader>cV",
               "<cmd>VtsExec select_ts_version<cr>",
               desc = "Select TS workspace version",
+            },
+            {
+              "gD",
+              function()
+                require("vtsls").commands.goto_source_definition(0)
+              end,
+              desc = "Goto Source Definition",
+            },
+            {
+              "gR",
+              function()
+                require("vtsls").commands.file_references(0)
+              end,
+              desc = "File References",
+            },
+            {
+              "<leader>co",
+              function()
+                require("vtsls").commands.organize_imports(0)
+              end,
+              desc = "Organize Imports",
+            },
+            {
+              "<leader>cM",
+              function()
+                require("vtsls").commands.add_missing_imports(0)
+              end,
+              desc = "Add missing imports",
+            },
+            {
+              "<leader>cu",
+              function()
+                require("vtsls").commands.remove_unused_imports(0)
+              end,
+              desc = "Remove unused imports",
+            },
+            {
+              "<leader>cD",
+              function()
+                require("vtsls").commands.fix_all(0)
+              end,
+              desc = "Fix all diagnostics",
             },
           },
 
