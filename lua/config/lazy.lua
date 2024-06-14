6ae1f7cf2eb3201f6e947850191c9f49ca17d567
Huynh Duc Dung <dunghd.it@gmail.com>
2024-06-14 21:48:29 +0800

    refactor(lazy.lua): simplify filesystem check and path prepend

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,10 +1,11 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
-vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = {
