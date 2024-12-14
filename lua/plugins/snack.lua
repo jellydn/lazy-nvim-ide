d4f702bc77c75548e4f66ea3cdeed7d7eb969960
Dung Huynh Duc <dunghd.it@gmail.com>
2024-12-14 21:30:48 +0800

    feat(snack): enable zen mode with customized settings

--- lua/plugins/snack.lua
+++ lua/plugins/snack.lua
@@ -4,6 +4,17 @@ return {
     optional = true,
     opts = {
       dashboard = { enabled = false },
+      zen = {
+        enabled = true,
+        win = {
+          -- Hide backdrop
+          backdrop = { transparent = false },
+        },
+        toggles = {
+          -- Turn off dim plugin for zen mode
+          dim = false,
+        },
+      },
     },
   },
 }
